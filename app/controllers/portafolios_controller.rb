fa89547a8504bbb1f2b492872c6068d334ee79bd
bnd <banda1915@gmail.com>
2020-01-28 17:59:23 -0600

    new and create for portafolio

--- app/controllers/portafolios_controller.rb
+++ app/controllers/portafolios_controller.rb
@@ -2,4 +2,20 @@ class PortafoliosController < ApplicationController
   def index
     @portafolio_items = Portafolio.all
   end
+
+  def new
+    @portafolio_item = Portafolio.new
+  end
+
+  def create
+    @portafolio_item = Portafolio.new(params.require(:portafolio).permit(:title, :subtitle, :body))
+
+    respond_to do |format|
+      if @portafolio_item.save
+        format.html { redirect_to portafolios_path, notice: 'Your portafolio is now live' }
+      else
+        format.hml { render :new }
+      end
+    end
+  end
 end
