a7b3e70c53e76ccdebe2ee8dc81a77e926ae340f
bnd <banda1915@gmail.com>
2020-01-29 20:52:00 -0600

    friendly

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,4 +1,4 @@
-lass BlogsController < ApplicationController
+class BlogsController < ApplicationController
   before_action :set_blog, only: [:show, :edit, :update, :destroy]
 
   # GET /blogs
@@ -64,7 +64,7 @@ lass BlogsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_blog
-      @blog = Blog.find(params[:id])
+      @blog = Blog.friendly.find(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
---
+++ db/migrate/20200130021256_ad_slug_to_blogs.rb
@@ -0,0 +1,4 @@
+class AdSlugToBlogs < ActiveRecord::Migration[6.0]
+  def change
+  end
+end
